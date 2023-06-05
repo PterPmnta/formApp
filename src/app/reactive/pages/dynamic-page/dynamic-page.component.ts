15661fb40c348c9d407a6b1ee478f25ee6816bde
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-06-05 08:29:05 -0500

    input form array

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
   templateUrl: './dynamic-page.component.html',
-  styleUrls: ['./dynamic-page.component.css']
+  styleUrls: ['./dynamic-page.component.css'],
 })
 export class DynamicPageComponent {
+  public dynamicForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.formBuilder.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  get listGames() {
+    return this.dynamicForm.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit() {
+    if (this.dynamicForm.invalid) {
+      this.dynamicForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.dynamicForm.value);
+    this.dynamicForm.reset();
+  }
 }
